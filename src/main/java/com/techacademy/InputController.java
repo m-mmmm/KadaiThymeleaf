0cf10722be3b9fda141814e0e5859f6d77251527
miki.kida <mikimikikk55@gmail.com>
2024-01-26 00:55:03 +0900

    Lesson15課題

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,18 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+    @Controller
+    public class InputController {
+
+    @GetMapping("/input")
+    public String getInput(@RequestParam(name = "aid", required = false) String aid, Model model){
+        // input.htmlに画面遷移
+        model.addAttribute("previous", aid);
+        return "input";
+    }
+
+}
---
+++ src/main/java/com/techacademy/OutputController.java
@@ -0,0 +1,18 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class OutputController {
+
+    @PostMapping("/output")
+    public String getOutput(@RequestParam("val") String val, Model model) {
+        model.addAttribute("id",val);
+        // getOutput.htmlに入力画面から入力値を受け取って、出力画面を表示する
+        return "output";
+    }
+}
